c02cc7f575ca99c317970f224fe05cce55786b0c
KacperPC <kacp.woja@gmail.com>
2019-04-16 03:49:10 +0200

    rosenbrock

--- multidimension/func_test.py
+++ multidimension/func_test.py
@@ -4,18 +4,21 @@ from mpl_toolkits import mplot3d
 from matplotlib import cm
 
 # Definitions
-start = [-10, -10]
-end = [10, 10]
+start = [-2, -1]
+end = [2, 3]
 
-simplex_fun = lambda x, y: x**2+y**2
+simplex_fun = lambda x: 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
 # -----------
 x = np.linspace(start[0], end[0])
 y = np.linspace(start[1], end[1])
 x, y = np.meshgrid(x, y)
 
-z = simplex_fun(x, y)
+xx = np.array([x, y])
+
+z = simplex_fun(xx)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot_surface(x,y,z, linewidth=0, cmap=cm.jet)
+ax.plot_surface(xx[0], xx[1], z, linewidth=0, cmap=cm.jet)
+
 plt.show()
